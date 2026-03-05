a765587cf0d6f9792a4072e8309291f98ed339a2
Milly <1390377170@qq.com>
2026-03-05 22:37:31 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/springbootdemo/repository/UserRepository.java
+++ src/main/java/com/example/springbootdemo/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.springbootdemo.repository;
 
 import com.example.springbootdemo.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
